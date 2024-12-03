8b701ed2d207290c3d8888de9c860aa82ebd7566
LeandroPom <leotrabajo02@gmail.com>
2024-12-02 21:52:32 -0300

    get tag by id - up

--- src/handlers/user/getUserByIdHandler.js
+++ src/handlers/user/getUserByIdHandler.js
@@ -2,7 +2,7 @@ const getUserById = require('../../controllers/user/getUserById');
 
 module.exports = async (req, res) => {
 
-  const { id } = req.params; // Cambia de req.query a req.params
+  const { id } = req.params;
   console.log(`Solicitud para obtener el usuario con ID: ${id}`);
 
   try {
