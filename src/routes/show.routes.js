284b4bf37970e1920fbc6680e5e3608d8e7a70c7
LeandroPom <leotrabajo02@gmail.com>
2024-12-09 18:42:44 -0300

    edit show

--- src/routes/show.routes.js
+++ src/routes/show.routes.js
@@ -3,10 +3,12 @@ const showRouter = Router();
 const getAllShows = require('../handlers/show/getAllShowsHandler');
 const createShow = require('../handlers/show/createShowHandler');
 const getShowById = require('../handlers/show/getShowByIdHandler');
+const editShow = require('../handlers/show/editShowHandler');
 
 
 showRouter.get('/', getAllShows);
 showRouter.get('/:id', getShowById);
+showRouter.put('/edit', editShow);
 showRouter.post('/', createShow);
 
 module.exports = showRouter;
