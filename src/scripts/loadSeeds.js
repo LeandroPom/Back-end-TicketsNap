614c9625b34a11144a2e81ed2a14907a28943350
LeandroPom <leotrabajo02@gmail.com>
2024-12-11 19:36:16 -0300

    seeder db false

--- src/scripts/loadSeeds.js
+++ src/scripts/loadSeeds.js
@@ -12,30 +12,60 @@ async function loadSeeds() {
     console.log('Cargando datos iniciales en la base de datos...');
 
     // // Usuarios
+    // const dbUser = await User.findAll();
+    // if (dbUser.length) {
+    //   console.log("existingUseres found");
+    //   return;
+    // }
+
     // const usersData = JSON.parse(fs.readFileSync(path.join(seedsPath, 'users.json'), 'utf-8'));
     // for (const userData of usersData) {
     //   await User.create(userData);
     // }
 
     // Lugares
+    const dbPlace = await Place.findAll();
+    if (dbPlace.length) {
+      console.log("existing places found");
+      return;
+    }
+
     const placesData = JSON.parse(fs.readFileSync(path.join(seedsPath, '../seeds/places.json'), 'utf-8'));
     for (const placeData of placesData) {
       await Place.create(placeData);
     }
 
     // Tags
+    const dbTag = await Tag.findAll();
+    if (dbTag.length) {
+      console.log("existing Tags found");
+      return;
+    }
+
     const tagsData = JSON.parse(fs.readFileSync(path.join(seedsPath, '../seeds/tags.json'), 'utf-8'));
     for (const tagData of tagsData) {
       await Tag.create(tagData);
     }
 
     // Shows
+    const dbShow = await Show.findAll();
+    if (dbShow.length) {
+      console.log("existing Shows found");
+      return;
+    }
+
     const showsData = JSON.parse(fs.readFileSync(path.join(seedsPath, '../seeds/shows.json'), 'utf-8'));
     for (const showData of showsData) {
       await Show.create(showData);
     }
 
     // // Zonas
+    // const dbZone = await Zone.findAll();
+    // if (dbZone.length) {
+    //   console.log("existing Zones found");
+    //   return;
+    // }
+
     // const zonesData = JSON.parse(fs.readFileSync(path.join(seedsPath, 'zones.json'), 'utf-8'));
     // for (const zoneData of zonesData) {
     //   await Zone.create(zoneData);
