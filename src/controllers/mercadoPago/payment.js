38bc511f63025a6b40c8e06852a4d71e4b6ab572
Leandro <112503474+LeandroPom@users.noreply.github.com>
2025-03-19 14:11:03 -0300

    Update payment.js

--- src/controllers/mercadoPago/payment.js
+++ src/controllers/mercadoPago/payment.js
@@ -4,7 +4,7 @@ const MP_ACCESS_TOKEN = process.env.MP_ACCESS_TOKEN;
 // console.log(MP_ACCESS_TOKEN)
 
 // Configurar Mercado Pago con el Access Token
-const client = new MercadoPagoConfig({ accessToken: MP_ACCESS_TOKEN, options: { timeout: 3000, idempotencyKey: 'abc' } });
+const client = new MercadoPagoConfig({ accessToken: MP_ACCESS_TOKEN, options: { timeout: 5000, idempotencyKey: 'abc' } });
 
 module.exports = async (ticketId, name, mail, phone, dni, price, zoneId, showId, description) => {
     try {
